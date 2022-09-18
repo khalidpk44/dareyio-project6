bdf4c584921c9c0f8e239523ce99cdf7c0f04bd0
khalidpk44 <100615007+khalidpk44@users.noreply.github.com>
2022-09-18 10:00:26 +0100

    Create webserver.sh

---
+++ webserver.sh
@@ -0,0 +1,23 @@
+sudo yum -y install wget httpd php php-mysqlnd php-fpm php-json
+sudo systemctl enable httpd
+sudo systemctl start httpd
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo yum install yum-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm
+sudo yum module list php
+sudo yum module reset php
+sudo yum module enable php:remi-7.4
+sudo yum install php php-opcache php-gd php-curl php-mysqlnd
+sudo systemctl start php-fpm
+sudo systemctl enable php-fpm
+setsebool -P httpd_execmem 1
+sudo systemctl restart httpd
+mkdir wordpress
+cd   wordpress
+sudo wget http://wordpress.org/latest.tar.gz
+sudo tar xzvf latest.tar.gz
+sudo rm -rf latest.tar.gz
+sudo cp wordpress/wp-config-sample.php wordpress/wp-config.php
+sudo cp -R wordpress /var/www/html/
+sudo chown -R apache:apache /var/www/html/wordpress
+sudo chcon -t httpd_sys_rw_content_t /var/www/html/wordpress -R
+sudo setsebool -P httpd_can_network_connect=1
